e78d025693ca3aafc4b1c471ab6780e8437a79cc
Karanveer Sidhu <karansidhu5550@gmail.com>
2024-11-23 17:41:56 -0800

    add more tests

--- frontend/src/pages_test/SignupPage.test.js
+++ frontend/src/pages_test/SignupPage.test.js
@@ -10,4 +10,9 @@ describe('handleSignup', () => {
   test('returns true if all fields are filled', () => {
     expect(handleSignup('JohnDoe', 'john@example.com', 'password123')).toBe(true);
   });
+
+  test('returns false if password is less than 6 characters', () => {
+    expect(handleSignup('Tim', 'tim@example.com', '123')).toBe(false)
+    expect(handleSignup('Tim', 'tim@example.com', 'timlikescake123')).toBe(true)
+  })
 });
